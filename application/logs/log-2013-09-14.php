056b8bdb5c711f37ed9a49ab07b11d3b68af97fc
Zlatko Micic <zlatkomicic@gmail.com>
2013-09-17 01:33:44 +0200

    old log file

---
+++ application/logs/log-2013-09-14.php
@@ -0,0 +1,28 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed'); ?>
+
+DEBUG - 2013-09-14 14:02:00 --> Config Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Hooks Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Utf8 Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> UTF-8 Support Enabled
+DEBUG - 2013-09-14 14:02:00 --> URI Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Router Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Output Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Security Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Input Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Global POST and COOKIE data sanitized
+DEBUG - 2013-09-14 14:02:00 --> Language Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Loader Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Helper loaded: url_helper
+DEBUG - 2013-09-14 14:02:00 --> Helper loaded: form_helper
+DEBUG - 2013-09-14 14:02:00 --> Database Driver Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Session Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Helper loaded: string_helper
+DEBUG - 2013-09-14 14:02:00 --> Session routines successfully run
+DEBUG - 2013-09-14 14:02:00 --> Controller Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Model Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Model Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> Form Validation Class Initialized
+DEBUG - 2013-09-14 14:02:00 --> File loaded: application/views/login_view.php
+DEBUG - 2013-09-14 14:02:00 --> File loaded: application/views/template.php
+DEBUG - 2013-09-14 14:02:00 --> Final output sent to browser
+DEBUG - 2013-09-14 14:02:00 --> Total execution time: 0.0934
